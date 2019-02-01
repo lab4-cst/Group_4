fff93c9266052e56a6fe8710a852d399fa6bf058
JasonZhouE <zhoujason97@gmail.com>
2019-02-01 14:11:14 -0800

    added controllers

---
+++ app/Controllers/students.php
@@ -0,0 +1,36 @@
+<?php namespace App\Controllers;
+
+use CodeIgniter\API\ResponseTrait;
+
+class students extends \CodeIgniter\Controller
+{
+    use ResponseTrait;
+
+    public function create()
+    {
+        $model = new UserModel();
+        $user  = $model->save($this->request->getPost());
+
+        // Respond with 201 status code
+        return $this->respondCreated();
+    }
+	public function index()
+	{
+		return students;
+	}
+	public function show($id){
+		return students($id);
+	}
+	public function edit($id){
+		return $this->fail('Not implemented',418);
+	}
+	public function update($id){
+		return $this->fail('Not implemented',418);
+	}
+	public function delete($id){
+		return $this->fail('Not implemented',418);
+	}
+	public function new(){
+		return $this->fail('Not implemented',418);
+	}
+}
