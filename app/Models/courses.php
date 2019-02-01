b4031f54290c54bd596d6b4d8a4d9d8e9435d8de
mlau95 <mattlau1218@gmail.com>
2019-02-01 14:30:56 -0800

    fixed indentations

--- app/Models/courses.php
+++ app/Models/courses.php
@@ -2,22 +2,22 @@
 
 class courses {
 
-        protected $data = [
-			    'Operating Systems' 		=> ['id'=>'Operating Systems', 'code'=>'COMP4735'],
-			    'Pitching and Positioning' 	=> ['id'=>'Pitching and Positioning', 'code'=>'BUSA4919'],
-			    'UX/UI Design' 				=> ['id'=>'UX/UI Design', 'code'=>'COMP4943']
-		];
+	protected $data = [
+		'Operating Systems' => ['id'=>'Operating Systems', 'code'=>'COMP4735'],
+		'Pitching and Positioning' => ['id'=>'Pitching and Positioning', 'code'=>'BUSA4919'],
+		'UX/UI Design' => ['id'=>'UX/UI Design', 'code'=>'COMP4943']
+	];
 
-        public function find($id)
-        {
-			return $this->data[$id]['code'];
-        }
+	public function find($id)
+	{
+		return $this->data[$id]['code'];
+	}
 
-        public function findAll()
-        {
-			foreach ($this->data as $key => $value) 
-			{
-                return $this->data[$key]['surname'];
-            }
-        }
+	public function findAll()
+	{
+		foreach ($this->data as $key => $value) 
+		{
+			return $this->data[$key]['surname'];
+		}
+	}
 }
--- app/Models/students.php
+++ app/Models/students.php
@@ -2,22 +2,22 @@
 
 class students {
 
-        protected $data = [
-			    'Parm' 		=> ['id'=>'Parm', 'surname'=>'Dhaliwal'],
-			    'Jason' 	=> ['id'=>'Jason', 'surname'=>'Zhou'],
-			    'Matthew' 	=> ['id'=>'Matthew', 'surname'=>'Lau']
-		];
+	protected $data = [
+		'Parm' => ['id'=>'Parm', 'surname'=>'Dhaliwal'],
+		'Jason' => ['id'=>'Jason', 'surname'=>'Zhou'],
+		'Matthew' => ['id'=>'Matthew', 'surname'=>'Lau']
+	];
 
-        public function find($id)
-        {
-			return $this->data[$id]['surname'];
-        }
+	public function find($id)
+	{
+		return $this->data[$id]['surname'];
+	}
 
-        public function findAll()
-        {
-			foreach ($this->data as $key => $value) 
-			{
-                return $this->data[$key]['surname'];
-            }
-        }
+	public function findAll()
+	{
+		foreach ($this->data as $key => $value) 
+		{
+			return $this->data[$key]['surname'];
+		}
+	}
 }
